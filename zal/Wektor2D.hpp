bbfba4e231e9ad15c84cdb011430f6d7526d085f
nojmar <73884817+nojmar@users.noreply.github.com>
2020-11-03 18:20:51 +0100

    Update Wektor2D.hpp

--- zal/Wektor2D.hpp
+++ zal/Wektor2D.hpp
@@ -1,4 +1,40 @@
 class Wektor2D
 {
-    // Tutaj napisz implementacje klasy Wektor2D
+    private:
+    double X;
+    double Y;
+   
+    public:
+    Wektor2D(double x, double y){
+        X = x;
+        Y = y;
+    };
+    
+    Wektor2D(){
+        X = 0;
+        Y = 0;
+    };
+    Wektor2D operator + (Wektor2D obj) {
+        Wektor2D result;
+        result.setX(obj.getX() + X);
+        result.setY(obj.getY() + Y);;
+        return result;
+    }
+    
+    double operator * (Wektor2D obj) {
+        double iloczynSkalarny = obj.getX() * X + obj.getY() * Y;
+        return iloczynSkalarny;
+    }
+    void setX(double value) {
+        X = value;
+    }
+    double getX() {
+        return X;
+    }
+    void setY(double value) {
+        Y = value;
+    }
+    double getY() {
+        return Y;
+    }
 };
